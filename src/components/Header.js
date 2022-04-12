a663830e6dbb953854ebda1245504f0c202fed6a
Rupi Sadhra <info@rupicodes.com>
2022-04-13 10:59:00 +1200

    Images and css styles added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { headerInfo } from "./data";
+import logoImage from "../images/logo.svg";
 
 const Header = () => {
   const [currentUser, setCurrentUser] = useState(0);
@@ -11,7 +12,8 @@ const Header = () => {
   return (
     <>
       <nav className="navbar">
-        <section>
+        <section class="menu-left">
+          <img src={logoImage}></img>
           <ul>{menuLinks}</ul>
         </section>
         <section>{headerInfo[currentUser].currentProfile}</section>
