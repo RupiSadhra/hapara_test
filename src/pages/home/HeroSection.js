db985da674ca162766de03d48737ec0a55eebbe1
Rupi Sadhra <info@rupicodes.com>
2022-04-14 09:08:25 +1200

    Home Hero Section made responsive

--- src/pages/home/HeroSection.js
+++ src/pages/home/HeroSection.js
@@ -3,7 +3,17 @@ import React from "react";
 const HeroSection = () => {
   return (
     <>
-      <h1>Hero Section</h1>
+      <div className="hero-section centered">
+        <section className="hero-text centered col">
+          <h1>Classroom management software from Hāpara</h1>
+          <p>
+            Face to face or remote, Hāpara powers awesome learning. The most
+            effective and efficient way to organize teaching and learning
+            workflows in Google Workspace seamlessly.
+          </p>
+        </section>
+        <div className="overlay"></div>
+      </div>
     </>
   );
 };
